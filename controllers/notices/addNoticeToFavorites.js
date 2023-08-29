47e08b05773d340b5fceb1242a06217f14ddeeb4
Sergey Shmatok <trycifer@gmail.com>
2023-08-29 22:07:57 +0300

    Update addNoticeToFavorites.js

--- controllers/notices/addNoticeToFavorites.js
+++ controllers/notices/addNoticeToFavorites.js
@@ -5,28 +5,35 @@ const { ctrlWrapper } = require("../../utils");
 const addNoticeToFavorites = async (req, res) => {
   const { id } = req.params;
   const { _id } = req.user;
-  const user = await User.findById(_id);
-  if (!user) {
-    throw HttpError(404, `User with  id "${_id}" not found `);
-  }
-  if (User.find({ favorite: id })) {
-    throw HttpError(
-      409,
-      `Notices with id "${id}" is already been added to your favorite`
+
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      throw HttpError(404, `User with id "${_id}" not found`);
+    }
+
+    if (user.favorite.includes(id)) {
+      throw HttpError(
+        409,
+        `Notice with id "${id}" is already in your favorites`
+      );
+    }
+
+    await User.findOneAndUpdate(
+      { _id },
+      { $push: { favorite: id } },
+      { new: true }
     );
+
+    res.status(200).json({
+      id: `${id}`,
+      message: "Successfully added to favorites",
+    });
+  } catch (error) {
+    res
+      .status(error.statusCode || 500)
+      .json({ error: error.message || "Internal Server Error" });
   }
-  user.findByIdAndUpdate(
-    _id,
-    {
-      $push: { favorite: { $each: [id], $position: 0 } },
-    },
-    { new: true }
-  );
-  await user.save();
-  res.status(200).json({
-    id: `${id}`,
-    message: "Successfully added to favorites",
-  });
 };
 
 module.exports = { addNoticeToFavorites: ctrlWrapper(addNoticeToFavorites) };
