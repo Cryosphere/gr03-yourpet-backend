11f5cf4ee57168361c5d488be0c0eeb6b2329ae2
Sergey Shmatok <trycifer@gmail.com>
2023-08-30 10:13:40 +0300

    Some

--- controllers/users/updateUser.js
+++ controllers/users/updateUser.js
@@ -5,6 +5,13 @@ const { cloudinaryAddImage } = require("../../middlewares");
 const updateUser = async (req, res) => {
   const { _id } = req.user;
 
+  const { email } = req.body;
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser && existingUser._id.toString() !== _id) {
+    return res.status(400).json({ error: "Email already exists" });
+  }
+
   let updatedFields = { ...req.body };
   if (req.file) {
     const imageURL = await cloudinaryAddImage(req.file.path);
